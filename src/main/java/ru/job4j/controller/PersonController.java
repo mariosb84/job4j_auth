8106e7d37ffbea01d2b4458bceb0cf9ada7db86a
Sergei Baranov <mariosb84@mail.ru>
2023-08-30 17:15:58 +0300

    5. Обработка исключений и Spring REST [#504797]

--- src/main/java/ru/job4j/controller/PersonController.java
+++ src/main/java/ru/job4j/controller/PersonController.java
@@ -1,12 +1,21 @@
 package ru.job4j.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import ru.job4j.domain.Person;
 import ru.job4j.service.PersonService;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/person")
@@ -14,8 +23,13 @@ public class PersonController {
 
     private final PersonService persons;
 
-    public PersonController(final PersonService persons) {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PersonController.class.getSimpleName());
+
+    private final ObjectMapper objectMapper;
+
+    public PersonController(final PersonService persons, ObjectMapper objectMapper) {
         this.persons = persons;
+        this.objectMapper = objectMapper;
     }
 
     @GetMapping("/")
@@ -26,15 +40,30 @@ public class PersonController {
     @GetMapping("/{id}")
     public ResponseEntity<Person> findById(@PathVariable int id) {
         var person = this.persons.findById(id);
+       /*
         return new ResponseEntity<Person>(
                 person.orElse(new Person()),
                 person.isPresent() ? HttpStatus.OK : HttpStatus.NOT_FOUND
         );
+       */
+        if (person.isPresent()) {
+            return new ResponseEntity<Person>(
+                    person.orElse(new Person()),
+                    HttpStatus.OK
+            );
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Объект не найден!");
     }
 
     @PostMapping("/")
     public ResponseEntity<Person> create(@RequestBody Person person) {
         var result = this.persons.add(person);
+        if (person.getLogin() == null || person.getPassword() == null) {
+            throw new NullPointerException("Login and password mustn't be empty");
+        }
+        if (person.getPassword().length() < 6) {
+            throw new IllegalArgumentException("Invalid password. Password length must be more than 5 characters.");
+        }
         return new ResponseEntity<Person>(
                 result.orElse(new Person()),
                 result.isPresent() ? HttpStatus.CREATED : HttpStatus.CONFLICT
@@ -46,7 +75,8 @@ public class PersonController {
         if ((this.persons.update(person))) {
             return ResponseEntity.ok().build();
         }
-        return ResponseEntity.notFound().build();
+       /* return ResponseEntity.notFound().build();*/
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Объект не обновлен!");
     }
 
     @DeleteMapping("/{id}")
@@ -56,7 +86,19 @@ public class PersonController {
         if ((this.persons.delete(person))) {
             return ResponseEntity.ok().build();
         }
-        return ResponseEntity.notFound().build();
+        /*return ResponseEntity.notFound().build();*/
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Объект не удален!");
+    }
+
+    @ExceptionHandler(value = { IllegalArgumentException.class })
+    public void exceptionHandler(Exception e, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+        response.setContentType("application/json");
+        response.getWriter().write(objectMapper.writeValueAsString(new HashMap<>() { {
+            put("message", e.getMessage());
+            put("type", e.getClass());
+        }}));
+        LOGGER.error(e.getLocalizedMessage());
     }
 
 }
